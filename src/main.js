8be6cce721c1260e006b98d20adaeac6dcff6e5a
Yadvendar Champawat <yadvendar@testbook.com>
2017-07-12 17:02:57 +0530

    restrict to testbook only

--- src/main.js
+++ src/main.js
@@ -7,8 +7,15 @@ const express = require('express');
 const app = express();
 
 app.get('/', async function(request, response) {
-  const dom = await new Renderer(request.query.url).extractDOM(request.query.dom).catch((err) => console.error(err));
-  response.send(dom);
+    var host = request.query.url && request.query.url.split("/")[2] || "";
+    var hostArr = host.split(".");
+    var domain = hostArr[hostArr.length - 2] || "";
+    if (domain == "testbook"){
+        const dom = await new Renderer(request.query.url).extractDOM(request.query.dom).catch((err) => console.error(err));
+        response.send(dom);
+    } else {
+        response.send("Not Allowed");
+    }
 });
 
 app.get('/_ah/health', (request, response) => response.send('OK'));
